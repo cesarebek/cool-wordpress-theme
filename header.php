7202bad7c2b40dc74104f44bcc2ca5986152def5
Cesare Bek <cesarebek1@gmail.com>
2021-02-02 15:16:06 +0100

    cool wordpress theme

---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" >
+  <head>
+    <meta charset="UTF-8" />
+    <meta content="IE=edge" http-equiv="X-UA-Compatible" />
+    <meta content="width=device-width,initial-scale=1" name="viewport" />
+    <meta content="description" name="description" />
+    <meta name="google" content="notranslate" />
+    <meta
+      content="Mashup templates have been developped by Orson.io team"
+      name="author"
+    />
+
+    <!-- Disable tap highlight on IE -->
+    <meta name="msapplication-tap-highlight" content="no" />
+
+    <link href="./assets/apple-touch-icon.png" rel="apple-touch-icon" />
+    <link href="./assets/favicon.ico" rel="icon" />
+
+    <title>Title page</title>
+
+    <link href="./style.css" rel="stylesheet" />
+  </head>
+
+  <body>
+    <!-- Add your content of header -->
+    <header>
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+
+          <div class="navbar-collapse">
+            <button type="button" class="close" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <ul class="nav navbar-nav navbar-right">
+              <li>
+                <a
+                  href="./index.html#contact-section-container"
+                  title=""
+                  class="btn btn-default navbar-btn"
+                  >Contact Us</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
